6f814318821fb426dd3f03423fe0ef813cc789f9
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-08-28 10:34:13 -0500

    refactor: PopupWithForm & Section, remove old submit logic

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Todo } from "../components/Todo.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import { Section } from "../components/Section.js";
+import { PopupWithForm } from "../components/PopupWithForm.js";
 
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
@@ -10,22 +11,29 @@ const generateTodo = (data) => {
 };
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopup = document.querySelector("#add-todo-popup");
+
+const addTodoPopup = new PopupWithForm("#add-todo-popup", (formValues) => {
+  const name = formValues.name;
+  const dateInput = formValues.date;
+
+  let date;
+  if (dateInput) {
+    date = new Date(dateInput);
+    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+  }
+
+  const values = { id: uuidv4(), name, date };
+  todoSection.addItem(generateTodo(values));
+  formValidator.resetValidation();
+  addTodoPopup.close();
+});
+
+addTodoPopup.setEventListeners();
+
 const addTodoForm = document.forms["add-todo-form"];
 
 const formValidator = new FormValidator(validationConfig, addTodoForm);
 formValidator.enableValidation();
-
-const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-
-const handleEscClose = (evt) => {
-  if (evt.key === "Escape") {
-    const opened = document.querySelector(".popup_visible");
-    if (opened) {
-      closeModal(opened);
-    }
-  }
-};
 
 let todoSection;
 
@@ -40,39 +48,6 @@ todoSection = new Section({
 
 todoSection.renderItems();
 
-const openModal = (modal) => {
-  modal.classList.add("popup_visible");
-  document.addEventListener("keydown", handleEscClose);
-};
-
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-  document.removeEventListener("keydown", handleEscClose);
-};
-
 addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopup);
+  addTodoPopup.open();
 });
-
-addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
-});
-
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  let date;
-  if (dateInput) {
-    date = new Date(dateInput);
-    date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-  }
-
-  const values = { id: uuidv4(), name, date };
-  todoSection.addItem(generateTodo(values));
-  formValidator.resetValidation();
-  closeModal(addTodoPopup);
-});
-
